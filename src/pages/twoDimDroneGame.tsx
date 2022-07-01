e19400eeaa4ceec3361cb1f1702962b9407c8ec5
Keunwoo Park <keunwoopark.524@gmail.com>
2022-07-01 17:12:07 +0900

    Add TwoDimDroneGameThreeScene

--- src/pages/twoDimDroneGame.tsx
+++ src/pages/twoDimDroneGame.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from "next"
+import TwoDimDroneGameThreeScene from "../components/TwoDimDroneGameThreeScene";
 
 const TwoDimDroneGame: NextPage = () => {
     return (
-        <div>TwoDimDroneGame view</div>
+        <div className="flex flex-col items-center justify-center w-1/2 mx-auto min-h-screen">
+            <h1 className="text-5xl">TwoDimDroneGame</h1>
+            <TwoDimDroneGameThreeScene height={512} width={512} />
+        </div>
     )
 }
 
