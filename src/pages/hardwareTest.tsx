c2c5a4f42ea159d89f0f3c311de397ea9a0cf62d
Keunwoo Park <keunwoopark.524@gmail.com>
2022-07-01 16:48:08 +0900

    Add hadware test and 2D drone game view

---
+++ src/pages/hardwareTest.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const HardwareTest: NextPage = () => {
+    return (
+        <div>Hardware Test view</div>
+    )
+}
+
+export default HardwareTest;
---
+++ src/pages/twoDimDroneGame.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+const TwoDimDroneGame: NextPage = () => {
+    return (
+        <div>TwoDimDroneGame view</div>
+    )
+}
+
+export default TwoDimDroneGame;
