e11def74bd9e8b194ed054724234fa542d1e6f36
Keunwoo Park <keunwoopark.524@gmail.com>
2022-07-15 11:42:42 +0900

    Add web serial scanning

--- src/pages/hardwareTest.tsx
+++ src/pages/hardwareTest.tsx
@@ -1,8 +1,30 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+
 
 const HardwareTest: NextPage = () => {
+    const [ports, setPorts] = useState<any[]>([]);
+
+    useEffect(() => {
+        if ("serial" in navigator) {
+            // The Web Serial API is supported.
+            console.log("Web Serial API supported");
+            
+          }
+    }, []);
+
+    function scanPorts() {
+        console.log("scanning ports");
+        navigator.serial.requestPort().then((ports: any) => {
+            console.log(ports);
+        });
+    }
+
     return (
-        <div>Hardware Test view</div>
+        <div>
+            <div>Hardware Test view</div>
+            <button onClick={scanPorts}>scan ports</button>
+        </div>
     )
 }
 
