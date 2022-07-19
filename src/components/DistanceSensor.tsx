e5d98607946b400922c9f53c5c0376f80490954d
Keunwoo Park <keunwoopark.524@gmail.com>
2022-07-19 13:17:52 +0900

    Add angle range to the sensors

--- src/components/DistanceSensor.tsx
+++ src/components/DistanceSensor.tsx
@@ -1,20 +1,26 @@
-import { Line } from "@react-three/drei";
+import { Line, Sphere } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { getMaxListeners } from "process";
 import { RefObject, useEffect, useState } from "react";
-import { Mesh, Raycaster, Vector3 } from "three";
+import { Mesh, Raycaster, Scene, Vector3 } from "three";
 
 interface IDistanceSensorProps {
     droneRef: RefObject<Mesh>;
     direction: Vector3;
     wallLayerNumber: number;
+    showRaycastLine: boolean;
+    angleRange: number;
 }
+
+const distanceFromDrone = 0.2;
+const numSubRays = 5;
 
 export default function DistanceSensor(props: IDistanceSensorProps) {
 
     const [raycaster, setRaycaster] = useState<Raycaster>();
-    const [droneWorldPosition, setDroneWorldPosition] = useState(new Vector3(0, 0, 0));
+    const origin = new Vector3();
     const [raycastHitPoint, setRaycastHitPoint] = useState(new Vector3(0, 0, 0));
+    const [raycastHitDistance, setRaycastHitDistance] = useState(Infinity);
     const [isHit, setIsHit] = useState(false);
 
     useEffect(() => {
@@ -40,36 +46,50 @@ export default function DistanceSensor(props: IDistanceSensorProps) {
         
         const droneWorldPosition = new Vector3();
         drone.getWorldPosition(droneWorldPosition);
-        setDroneWorldPosition(droneWorldPosition.clone());
+        
+        const {hitDistance, hitDirection} = raycastInAngleRange(raycaster, droneWorldPosition, props.direction, props.angleRange, state.scene);
+        const raycastHitPoint = hitDirection.clone().multiplyScalar(hitDistance);
+        // raycaster.set(droneWorldPosition.clone(), props.direction.clone());
+        // const intersects = raycaster.intersectObjects(state.scene.children);
 
-        raycaster.set(droneWorldPosition.clone(), props.direction.clone());
-        const intersects = raycaster.intersectObjects(state.scene.children);
+        // let hitDistance = Infinity;
+        // let raycastHitPoint: Vector3 | null = null;
+        // let isHit = false;
+        // let raycastHitDistance = Infinity;
+        // if (intersects.length > 0) {
+        //     intersects.sort((a, b) => a.distance - b.distance);
+        //     const closestIntersect = intersects[0]!;
+        //     if (closestIntersect.distance != null && closestIntersect?.distance < Infinity && closestIntersect?.point != null) {
+        //         raycastHitPoint = closestIntersect?.point!.clone();
+        //         hitDistance = closestIntersect.distance;
+        //         isHit = true;
+        //     }
+        // }
 
-        let hitDistance = Infinity;
-        let raycastHitPoint: Vector3 | null = null;
-        let isHit = false;
-        if (intersects.length > 0) {
-            intersects.sort((a, b) => a.distance - b.distance);
-            const closestIntersect = intersects[0]!;
-            if (closestIntersect.distance != null && closestIntersect?.distance < Infinity && closestIntersect?.point != null) {
-                raycastHitPoint = closestIntersect?.point!.clone();
-                hitDistance = closestIntersect.distance;
-                isHit = true;
-            }
-        }
-
+        setRaycastHitDistance(hitDistance);
+        const isHit = hitDistance < Infinity;
         setIsHit(isHit);
 
-        if (raycastHitPoint == null) {
-            setRaycastHitPoint(new Vector3(0, 0, 0));
-        } else {
+        if (isHit) {
             setRaycastHitPoint(raycastHitPoint);
+        } else {
+            setRaycastHitPoint(new Vector3(0, 0, 0));
         }
     });
 
     function getLine() {
+        if(props.showRaycastLine && isHit) {
+            return <Line points={[origin, raycastHitPoint]} color={"pink"} lineWidth={2} ></Line>
+        } else {
+            return <></>;
+        }
+    }
+
+    function getSphere() {
         if(isHit) {
-            return <Line points={[droneWorldPosition, raycastHitPoint]} color={"pink"} lineWidth={2} ></Line>
+            return <Sphere args={[0.02]} position={props.direction.clone().multiplyScalar(distanceFromDrone)}> 
+                <meshBasicMaterial color={"red"} />
+            </Sphere>
         } else {
             return <></>;
         }
@@ -77,5 +97,32 @@ export default function DistanceSensor(props: IDistanceSensorProps) {
 
     return <>
         {getLine()}
+        {getSphere()}
     </>;
+}
+
+function raycastInAngleRange(raycaster: Raycaster, droneWorldPos: Vector3, direction: Vector3, angleRange: number, scene: Scene): {hitDistance: number, hitDirection: Vector3} {
+    const subDirections: Vector3[] = [];
+
+    const zAxis = new Vector3(0, 0, 1);
+    const midAngle = angleRange / 2;
+    for (let i = 0; i < numSubRays; i++) {
+        const rotAngle = angleRange / numSubRays * i - midAngle;
+        const subDir = direction.clone().applyAxisAngle(zAxis, rotAngle);
+        subDirections.push(subDir);
+    }
+
+    for (const subDir of subDirections) {
+        raycaster.set(droneWorldPos.clone(), subDir.clone());
+        const intersects = raycaster.intersectObjects(scene.children);
+        if (intersects.length > 0) {
+            intersects.sort((a, b) => a.distance - b.distance);
+            const closestIntersect = intersects[0]!;
+            if (closestIntersect.distance != null && closestIntersect?.distance < Infinity && closestIntersect?.point != null) {
+                return {hitDistance: closestIntersect.distance, hitDirection: subDir};
+            }
+        }
+    }
+
+    return {hitDistance: Infinity, hitDirection: new Vector3()};
 }
--- src/components/Drone.tsx
+++ src/components/Drone.tsx
@@ -16,9 +16,6 @@ export default function Drone(props: IDroneProps) {
     const numProbes = 8;
     const droneSize = 0.15;
     const [controlState, setControlState] = useState({forward: 0, backward: 0, right: 0, left: 0});
-    const [droneWorldPosition, setDroneWorldPosition] = useState(new Vector3(0, 0, 0));
-    const [raycastHitPoints, setRaycastHitPoints] = useState<Vector3[]>([]);
-    const [raycastHitDistances, setRaycastHitDistances] = useState<number[]>([]);
     const [droneCollilde, setDroneCollilde] = useState(false);
 
     const droneArgs: SphereArgs = [droneSize];
@@ -28,8 +25,8 @@ export default function Drone(props: IDroneProps) {
                                                         type: "Kinematic",
                                                         args: droneArgs,
                                                         collisionResponse: true,
-                                                        onCollideBegin: (e) => {setDroneCollilde(true), console.log("collide begin")},
-                                                        onCollideEnd: (e) => {setDroneCollilde(false), console.log("collide end")},
+                                                        onCollideBegin: (e) => {setDroneCollilde(true)},
+                                                        onCollideEnd: (e) => {setDroneCollilde(false)},
                                                     }));
     const probes: JSX.Element[] = [];
     const probeRefs: any[] = [];
@@ -52,8 +49,13 @@ export default function Drone(props: IDroneProps) {
         const y = Math.sin(angle) * probDist;
         const direction = new Vector3(x, y, 0);
         direction.normalize();
-
-        distanceSensors.push(<DistanceSensor key={`sensor-${i}`} droneRef={droneRef} wallLayerNumber={props.wallLayerNumber} direction={direction} />);
+        
+        distanceSensors.push(<DistanceSensor key={`sensor-${i}`} 
+                                            droneRef={droneRef} 
+                                            wallLayerNumber={props.wallLayerNumber} 
+                                            direction={direction} 
+                                            showRaycastLine={true} 
+                                            angleRange={2 * Math.PI / numProbes} />);
     }
 
     useFrame((state) => {
@@ -61,40 +63,9 @@ export default function Drone(props: IDroneProps) {
         state.camera.position.y -= controlState.backward * speed;
         state.camera.position.x += controlState.right * speed;
         state.camera.position.x -= controlState.left * speed;
-        const raycastHitPoints: Vector3[] = [];
-        const raycastHitDistances: number[] = [];
-        
+
         const newDronePosition = new Vector3(state.camera.position.x, state.camera.position.y, 0);
         droneApi.position.copy(newDronePosition);
-
-
-        // const drone = droneRef.current as Mesh;
-        
-        // const droneWorldPosition = new Vector3();
-        // drone.getWorldPosition(droneWorldPosition);
-        // setDroneWorldPosition(droneWorldPosition.clone());
-        // const raycaster = state.raycaster;
-        // raycaster.camera = state.camera;
-        // raycaster.layers.set(props.wallLayerNumber);
-
-        // for (const direction of sensorDirections) {
-        //     raycaster.set(droneWorldPosition.clone(), direction.clone());
-
-        //     const intersects = raycaster.intersectObjects(state.scene.children);
-        //     let hitDistance = Infinity;
-        //     if (intersects.length > 0) {
-        //         intersects.sort((a, b) => a.distance - b.distance);
-        //         const closestIntersect = intersects[0]!;
-        //         if (closestIntersect.distance != null && closestIntersect?.distance < Infinity && closestIntersect?.point != null) {
-        //             raycastHitPoints.push(closestIntersect?.point!.clone());
-        //             hitDistance = closestIntersect.distance;
-        //         }
-        //     }
-        //     raycastHitDistances.push(hitDistance);
-        // }
-
-        // setRaycastHitDistances(raycastHitDistances);
-        // setRaycastHitPoints(raycastHitPoints);
 
     });
 
@@ -141,62 +112,8 @@ export default function Drone(props: IDroneProps) {
                 <mesh ref={droneRef}>
                     <sphereGeometry args={droneArgs}/>
                     <meshBasicMaterial attach="material" color={droneCollilde? "red" : "blue"} />
+                    {distanceSensors}
                 </mesh>
-                {distanceSensors}
                 {/* <RayCastLineGroup center={droneWorldPosition} points={raycastHitPoints} /> */}
             </>);
-}
-
-interface IRaycastLineGroupProps {
-    center: Vector3;
-    points: Vector3[];
-}
-
-function RayCastLineGroup(props: IRaycastLineGroupProps) {
-    
-    function getLines() {
-        const lines: JSX.Element[] = [];
-        for (let i = 0; i < props.points.length; i++) {
-            const point = props.points[i]!;
-            const line = <Line key={`raycastline-${i}`} points={[props.center, point]} color={"pink"} lineWidth={2} ></Line>
-            lines.push(line);
-        }
-
-        return lines;
-    }
-
-    return (<>{getLines()}</>);
-}
-
-interface ISphereVizGroupProps {
-    sensorDirections: Vector3[];
-    sensorDistances: number[];
-    distanceFromDrone: number;
-}
-
-function SphereVizGroup(props: ISphereVizGroupProps) {
-    const minSphereSize = 0.01;
-    const maxSphereSize = 0.1;
-    const maxDistance = 100;
-    function distanceToSize(distance: number): number {
-        if (distance == Infinity) {
-            return minSphereSize;
-        }
-
-        const size = maxSphereSize * (maxDistance - distance) / maxDistance;
-        return Math.min(size, maxSphereSize);
-    }
-
-    function getSpheres() {
-        const spheres: JSX.Element[] = [];
-        for(let i = 0; i < props.sensorDirections.length; i++) {
-            const direction = props.sensorDirections[i]!;
-            const distance = props.sensorDistances[i]!;
-            const size = distanceToSize(distance);
-            const sphere = <Sphere key={`viz-sphere-${i}`} args={[size]} position={direction.clone().multiplyScalar(props.distanceFromDrone)}></Sphere>
-            spheres.push(sphere);
-        }
-    }
-
-    return (<>{getSpheres()}</>);
 }
