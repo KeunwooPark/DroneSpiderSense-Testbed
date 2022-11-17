b43d55f7d4a874781764617076983288d06b6117
Keunwoo Park <keunwoopark.524@gmail.com>
2022-11-17 19:02:18 +0900

    update the tile

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,20 +3,27 @@ import { useLoader } from "@react-three/fiber";
 import { Mesh, TextureLoader, Vector2 } from "three";
 
 interface ICellProps {
-    cellSize: number;
-    cellThickness: number;
-    layerNumber: number;
-    position: Vector2;
+  cellSize: number;
+  cellThickness: number;
+  layerNumber: number;
+  position: Vector2;
 }
 
 export default function Cell(props: ICellProps) {
-    const { cellSize, cellThickness, layerNumber, position } = props;
-    const [cellRef] = useBox<Mesh>(() => ({ mass: 1, position: [position.x, position.y, 0], type: "Static", args: [cellSize, cellSize, cellThickness] }));
+  const { cellSize, cellThickness, layerNumber, position } = props;
+  const [cellRef] = useBox<Mesh>(() => ({
+    mass: 1,
+    position: [position.x, position.y, 0],
+    type: "Static",
+    args: [cellSize, cellSize, cellThickness],
+  }));
 
-    const colorMap = useLoader(TextureLoader, "/textures/wall.jpg");
-    
-    return (<mesh ref={cellRef} layers={layerNumber}>
-        <boxGeometry args={[cellSize, cellSize, cellThickness]} />
-        <meshStandardMaterial attach="material" map={colorMap} />
-    </mesh>);
+  const colorMap = useLoader(TextureLoader, "/textures/green-tile.jpg");
+
+  return (
+    <mesh ref={cellRef} layers={layerNumber}>
+      <boxGeometry args={[cellSize, cellSize, cellThickness]} />
+      <meshStandardMaterial attach="material" map={colorMap} />
+    </mesh>
+  );
 }
