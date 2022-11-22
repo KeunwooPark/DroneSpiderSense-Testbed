94e596a4467e5e91c2264b0d4ca85bd85910d88b
Keunwoo Park <keunwoopark.524@gmail.com>
2022-11-22 16:36:48 +0900

    make inside of the cells black

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,6 +2,7 @@ import { useBox } from "@react-three/cannon";
 import { useLoader } from "@react-three/fiber";
 import { Mesh, TextureLoader, Vector2 } from "three";
 import * as THREE from "three";
+import { Box } from "@react-three/drei";
 
 interface ICellProps {
   cellSize: number;
@@ -12,19 +13,48 @@ interface ICellProps {
 
 export default function Cell(props: ICellProps) {
   const { cellSize, cellThickness, layerNumber, position } = props;
+  const smallOffset = 0.001;
   const [cellRef] = useBox<Mesh>(() => ({
     mass: 1,
     position: [position.x, position.y, 0],
     type: "Static",
-    args: [cellSize, cellSize, cellThickness],
+    args: [
+      cellSize - smallOffset,
+      cellSize - smallOffset,
+      cellThickness - smallOffset,
+    ],
   }));
+
+  // const [backsideCellRef] = useBox<Mesh>(() => ({
+  //   mass: 1,
+  //   position: [position.x, position.y, 0],
+  //   type: "Static",
+  //   args: [cellSize, cellSize, cellThickness],
+  // }));
 
   const colorMap = useLoader(TextureLoader, "/textures/green-tile.jpg");
 
   return (
-    <mesh ref={cellRef} layers={layerNumber}>
-      <boxGeometry args={[cellSize, cellSize, cellThickness]} />
-      <meshStandardMaterial attach="material" map={colorMap} />
-    </mesh>
+    <>
+      <mesh ref={cellRef} layers={layerNumber}>
+        <boxGeometry args={[cellSize, cellSize, cellThickness]} />
+        <meshStandardMaterial attach="material" map={colorMap} />
+      </mesh>
+      <Box
+        layers={layerNumber}
+        position={[position.x, position.y, 0]}
+        args={[
+          cellSize - smallOffset,
+          cellSize - smallOffset,
+          cellThickness - smallOffset,
+        ]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="black"
+          side={THREE.BackSide}
+        />
+      </Box>
+    </>
   );
 }
